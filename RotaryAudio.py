5f097a0129f90bd70ef5dcf26f94096721451a71
Alden Davidson <adavidson@protonmail.ch>
2019-11-11 08:39:35 -0800

    changed back file extensions, since PyAudio supports more than wav

--- RotaryAudio.py
+++ RotaryAudio.py
@@ -7,20 +7,20 @@ import simpleaudio as sa
 audiolocation = Path("/home/alden/Musik")
 
 audio = {
-    "dialtone":"US_dial_tone.wav",
-    "operator":"operatorTone.wav",
-    "1031":"Spooky Scary Skeletons (Remix) - Extended Mix.wav",
-    "1003":"German_national_anthem_performed_by_the_US_Navy_Band.wav",
-    "2346":"C418 - Minecraft - Volume Alpha - 22 Beginning.wav",
-    "2418":"C418 - Minecraft - Volume Alpha - 08 Minecraft.wav",
-    "2455":"might quit - Bill Wurtz.wav",
-    "3666":"Doot - E1M1 [Knee-Deep in the Doot].wav",
-    "7346":"Rammstein - Untitled - Radio.wav",
-    "7936":"C418 - Minecraft - Volume Alpha - 18 Sweden.wav",
-    "9253":"wakeUp.wav",
-    "0317":"The Wolfe Tones - Come Out Ye Black And Tans.wav",
-    "0501":"Gimn_Sovetskogo_Soyuza_(1944_Stalinist_lyrics).wav",
-    "0704":"Star_Spangled_Banner_instrumental.wav"
+    "dialtone":"US_dial_tone.ogg",
+    "operator":"operatorTone.m4a",
+    "1031":"Spooky Scary Skeletons (Remix) - Extended Mix.mp3",
+    "1003":"German_national_anthem_performed_by_the_US_Navy_Band.ogg",
+    "2346":"C418 - Minecraft - Volume Alpha - 22 Beginning.mp3",
+    "2418":"C418 - Minecraft - Volume Alpha - 08 Minecraft.mp3",
+    "2455":"might quit - Bill Wurtz.mp3",
+    "3666":"Doot - E1M1 [Knee-Deep in the Doot].mp3",
+    "7346":"Rammstein - Untitled - Radio.mp3",
+    "7936":"C418 - Minecraft - Volume Alpha - 18 Sweden.mp3",
+    "9253":"wakeUp.m4a",
+    "0317":"The Wolfe Tones - Come Out Ye Black And Tans.mp3",
+    "0501":"Gimn_Sovetskogo_Soyuza_(1944_Stalinist_lyrics).oga",
+    "0704":"Star_Spangled_Banner_instrumental.ogg"
 }
 
 if __name__ == "__main__":
