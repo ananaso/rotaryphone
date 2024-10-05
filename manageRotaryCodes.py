7c05294c2e89736ccaa7d88685e1ca984cf8b035
Alden Davidson <adavidson+github@protonmail.com>
2024-10-05 09:50:12 -0400

    chore: add python-cli-menu library to build menus

--- manageRotaryCodes.py
+++ manageRotaryCodes.py
@@ -4,8 +4,12 @@ Manage the dictionary of audio files defined in RotaryAudio.py, which defines
 the mapping between audio files and the code that plays them in rotary.py
 """
 
+from pythonclimenu import menu
 from RotaryAudio import audio
 
+OPTIONS = ["List current mappings", "Quit"]
+
+mainMenu = menu(title="Manage RotaryPi Audio Code Mappings", options=Options)
+
 if __name__ == "__main__":
-    print("Manager")
     print(audio)
