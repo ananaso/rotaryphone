1be2893b3b7c7bb41f11867b4734c3833ffd8737
Alden Davidson <adavidson+github@protonmail.com>
2024-10-05 18:34:32 -0400

    feat: list current audio mappings in a pretty format

--- manageRotaryCodes.py
+++ manageRotaryCodes.py
@@ -4,12 +4,38 @@ Manage the dictionary of audio files defined in RotaryAudio.py, which defines
 the mapping between audio files and the code that plays them in rotary.py
 """
 
+from enum import Enum
 from pythonclimenu import menu
 from RotaryAudio import audio
 
-OPTIONS = ["List current mappings", "Quit"]
+class MainMenuOptions(Enum):
+    LIST = "List current mappings"
+    QUIT = "Quit"
 
-mainMenu = menu(title="Manage RotaryPi Audio Code Mappings", options=Options)
+def list_current_mappings():
+    print("Current code mappings are:")
+    for code, filename in audio.items():
+        # ignore internal codes (which use strings)
+        if (len(code) == 4):
+            print("\t", code, " => ", filename)
+    input("Press Enter to continue...")
+
+
+def main_menu():
+    while True:
+        mainMenu = MainMenuOptions(menu(
+            title="Manage RotaryPi Audio Code Mappings", 
+            options=[e.value for e in MainMenuOptions], 
+            cursor_color="white"
+        ))
+        match mainMenu:
+            case MainMenuOptions.LIST:
+                list_current_mappings()
+            case MainMenuOptions.QUIT:
+                break
+            case _:
+                print("Error: Missing case!")
+                break
 
 if __name__ == "__main__":
-    print(audio)
+    main_menu()
